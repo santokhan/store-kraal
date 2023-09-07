eb6eb6f742bf96f6a9f678d9a0d7adff262fe6e9
Andrea Mura <43644548+andreamura@users.noreply.github.com>
2023-09-07 16:37:00 +0200

    Added API services

---
+++ src/services/api.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import * as firebase from "@/services/firebase";
+
+console.log(`Loading API. Base URL: ${import.meta.env.VITE_API_URL}`);
+const client = axios.create({
+     baseURL: import.meta.env.VITE_API_URL,
+     headers: {
+         'Content-Type': 'application/json',
+         'Accept': 'application/json',
+     },
+});
+client.interceptors.request.use(function (config) {
+     config.headers!.Authorization = `Bearer ${firebase.token}`;
+     return config;
+});
+
+async function get(url: string, data?: Record<string, unknown>) {
+     return client.get(url, data).then(res => res.data);
+}
+
+async function post(url: string, data?: Record<string, unknown>) {
+     return client.post(url, data).then(res => res.data);
+}
+
+export default {
+     main: {
+          getApiVersion: () => get('/'),
+     },
+     chat: {
+          getChats: () => get('/chats'),
+          getChat: (id: number) => get('/chat', { id: id }),
+          createChat: (name: string) => post('/chat', { name: name }),
+
+          getChatMessages: (chatId: number) => get('/chatmessages', { chatid: chatId }),
+          getChatMessage: (id: number) => get('/chatmessage', { id: id }),
+          sendChatMessage: (chatId: number, message: string) => post('/chatmessages', { chatid: chatId, message: message }),
+     },
+}
