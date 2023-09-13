f5b002346d494c4126dc355066f6078f7819e32d
Andrea Mura <43644548+andreamura@users.noreply.github.com>
2023-09-13 18:49:34 +0200

    Changing the environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const viteEnv = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith(`VITE_`)) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
+  }
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: viteEnv,
   plugins: [vue()],
 })
