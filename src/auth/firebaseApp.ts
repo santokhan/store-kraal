b463490bc2226b13f23dc5c684583b27782808fa
Andrea Mura <43644548+andreamura@users.noreply.github.com>
2023-09-13 17:59:32 +0200

    Changing the environment variables

--- src/auth/firebaseApp.ts
+++ src/auth/firebaseApp.ts
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_SERVICE_CREDENTIALS);
+const firebaseConfig = JSON.parse(process.env.VITE_FIREBASE_SERVICE_CREDENTIALS);
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
