38e0cf5bb611a52a54da3c8f3fe741d37fdeca05
Andrea Mura <43644548+andreamura@users.noreply.github.com>
2023-09-18 16:02:49 +0200

    Added Azure environment variables

--- src/auth/firebaseApp.ts
+++ src/auth/firebaseApp.ts
@@ -6,7 +6,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG)
+const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_SERVICE_CREDENTIALS)
 
 // Initialize Firebase App and export
 export const firebaseApp = initializeApp(firebaseConfig);
