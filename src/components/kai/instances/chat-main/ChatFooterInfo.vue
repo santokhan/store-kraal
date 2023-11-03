1de84bc51b989ff5ce2ecaa8fbf9fa94fd9f9b26
Santo <santokhan1999@gmail.com>
2023-11-03 19:41:08 +0600

    Fix: Kraal AI footer text

--- src/components/kai/instances/chat-main/ChatFooterInfo.vue
+++ src/components/kai/instances/chat-main/ChatFooterInfo.vue
@@ -1,3 +1,5 @@
 <template>
-    <p class="text-xs text-center px-4 py-3 text-gray-300">KraalAI may produce inaccurate information about certain facts.</p>
+    <p class="text-xs text-center px-4 py-2 lg:py-3 text-gray-300">
+        KraalAI may produce inaccurate information about certain facts.
+    </p>
 </template>
