212429db377325bcd4746c007dc672fc886f6838
Santo <santokhan1999@gmail.com>
2023-11-04 12:47:49 +0600

    Update: Kraal AI footer text

--- src/components/kai/instances/chat-main/ChatFooterInfo.vue
+++ src/components/kai/instances/chat-main/ChatFooterInfo.vue
@@ -1,5 +1,5 @@
 <template>
-    <p class="text-xs text-center px-4 py-2 lg:py-3 text-gray-300">
-        KraalAI may produce inaccurate information about certain facts.
+    <p class="text-xs text-center px-4 py-2 lg:py-3 text-gray-300 w-full whitespace-nowrap overflow-hidden text-ellipsis">
+        AI may produce inaccurate information about certain facts.
     </p>
 </template>
