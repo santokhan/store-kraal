95eceaab9dcdb87e8118d50e0d7d186dd1d9c745
Santo <santokhan1999@gmail.com>
2023-11-27 14:25:19 +0600

    Update Sidebar UI

--- src/components/kai/sidebar/ChatSidebar.vue
+++ src/components/kai/sidebar/ChatSidebar.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="absolute lg:relative z-[2] lg:z-[1]">
-		<ChatSideBarToggler @click="store.toggleSideBarDesktop" />
+		<div class="absolute left-full top-1/2 -translate-y-1/2 z-[11]">
+			<ChatSideBarToggler @click="store.toggleSideBarDesktop" :isOpen="store.getSidebar()" />
+		</div>
 		<SidebarContainer :class="dyClass(state.sidebarDesktop)">
 			<NewChat />
 			<ChatSideBarNavs />
@@ -10,17 +12,17 @@
 </template>
 
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
-import { useMediaQuery } from '@vueuse/core'
-import ChatSideBarToggler from './ChatSideBarToggler.vue'
-import SidebarContainer from './SidebarContainer.vue'
-import ChatSideBarNavs from './sidebar-navs/ChatSideBarNavs.vue'
-import NewChat from './left/NewChat.vue'
-import SidebarFooter from './SidebarFooter.vue'
-import { useChatSideBarStore } from '../../../stores/chatSideBar'
+import { storeToRefs } from 'pinia';
+import { useMediaQuery } from '@vueuse/core';
+import ChatSideBarToggler from './ChatSideBarToggler.vue';
+import SidebarContainer from './SidebarContainer.vue';
+import ChatSideBarNavs from './sidebar-navs/ChatSideBarNavs.vue';
+import NewChat from './left/NewChat.vue';
+import SidebarFooter from './SidebarFooter.vue';
+import { useChatSideBarStore } from '../../../stores/chatSideBar';
 
-const store = useChatSideBarStore()
-const { state } = storeToRefs(store)
+const store = useChatSideBarStore();
+const { state } = storeToRefs(store);
 
 function checkScreenSize() {
 	// tailwind lg `"min-width: 1024px"`
--- src/components/kai/sidebar/right/Container.vue
+++ src/components/kai/sidebar/right/Container.vue
@@ -1,15 +1,9 @@
 <template>
     <aside class="bg-chatgpt-600 text-white fixed lg:relative right-0 top-0 bottom-0">
-        <button type="button" @click="isOpen = !isOpen" :class="[
-            'flex items-center justify-center pl-1',
-            'h-[2.75rem] min-h-[2.75rem] w-[1.5rem] min-w-[1.5rem] absolute right-full top-1/2 -translate-y-1/2 z-[11]',
-            'transition-colors duration-200 text-white text-sm rounded-l-full bg-chatgpt-600 z-10'
-        ]"><i
-                :class="['fa fa-angle-right transition-transform linear text-lg', isOpen ? 'scale-x-100' : '-scale-x-100']"></i>
-        </button>
-
-        <div v-if="isOpen" class="w-72 lg:w- [320px] lg:w-[364px] p-4"
-            :class="props.view === 'GRID' ? 'gap-4' : 'gap-3'">
+        <div class="absolute right-full top-1/2 -translate-y-1/2 z-[11]">
+            <ChatSideBarToggler @click="isOpen = !isOpen" :isOpen="isOpen" :flip="true" />
+        </div>
+        <div v-if="isOpen" class="w-72 lg:w-[364px] p-4" :class="props.view === 'GRID' ? 'gap-4' : 'gap-3'">
             <slot></slot>
         </div>
     </aside>
@@ -17,6 +11,8 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import ChatSideBarToggler from '../ChatSideBarToggler.vue';
+
 const isOpen = ref(false)
 const props = defineProps<{ view: string }>()
 </script>
