5d6247dde389f52bd44bbdbb9ccd058211c0ad48
Santo <santokhan1999@gmail.com>
2023-11-30 00:26:39 +0600

    Update: Attachment icon

--- src/components/kai/instances/chat-main/FileInputBox.vue
+++ src/components/kai/instances/chat-main/FileInputBox.vue
@@ -3,12 +3,13 @@
         'w-[2rem] max-w-[2rem] h-[2rem] max-h-[2rem] flex justify-center items-center overflow-hidden',
         'text-gray-400 hover:bg-chatgpt-500 rounded-lg relative'
     ]">
-        <Attachment class="w-5 h-5" />
+        <DocumentIcon class="w-5 h-5" />
         <slot></slot>
     </div>
 </template>
 
 <script setup lang="ts">
+import { DocumentIcon } from '@heroicons/vue/24/outline';
 import Attachment from '../../../icons/attachment.vue';
 </script>
 
