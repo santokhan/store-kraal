24bb4bc58268ceea7d88fbbbd2d130640c61e1af
Santo <santokhan1999@gmail.com>
2023-11-30 14:38:50 +0600

    Clean: Sidebar toggler

--- src/components/kai/sidebar/ChatSideBarToggler.vue
+++ src/components/kai/sidebar/ChatSideBarToggler.vue
@@ -15,7 +15,6 @@ const props = defineProps<Props>()
 <template>
     <button type="button" :class="['relative w-6', props.flip ? '-scale-x-100' : '']">
         <svg class='w-full fill-chatgpt-600' viewBox="0 0 40 160" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <!-- width="160" height="40" -->
             <path
                 d="M6.35572e-07 160L7.62939e-06 -5.56315e-06C5.88094e-06 40 40 40 40 80C40 120 2.38403e-06 120 6.35572e-07 160Z"
                 fill="fillColor" />
