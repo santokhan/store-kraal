8ba89a52803fbff4a53d290bfc882a8efd97b6e9
Santo <santokhan1999@gmail.com>
2023-12-05 21:45:01 +0600

    Update: Simplify delete modal

--- src/components/kai/sidebar/sidebar-navs/items/DeleteModal.vue
+++ src/components/kai/sidebar/sidebar-navs/items/DeleteModal.vue
@@ -15,28 +15,17 @@
                 <hr class="border-gray-700">
                 <div class="flex flex-col items-center gap-4 py-10">
                     <Kraal class="w-20 select-none" />
-                    <h2 class="w-full text-2xl font-semibold text-center select-none">Are you sure to delete?</h2>
+                    <h2 class="w-full text-xl font-semibold text-center select-none">Are you sure to delete?</h2>
                 </div>
                 <hr class="border-gray-700">
-                <form @submit="handleSubmit">
-                    <div class="space-y-2 p-4">
-                        <label for="chatName" class="font-medium text-sm select-none">
-                            To confirm type "<span class="select-text">
-                                {{ props.chat.name.split(' ').slice(0, 5).join(" ") }}...
-                            </span>" in the box below
-                        </label>
-                        <input type="text" name="chat_name" id="chatName" v-model="name"
-                            class="block px-3 w-full h-9 text-gray-400 text-base rounded-lg border bg-transparent outline-none border-orange-900 text-sm hover:border-orange-800">
-                        <div v-if="err" class="text-sm flex items-center gap-1 text-orange-700">
-                            <i class="fa fa-info-circle"></i>Typing error. Check again.
-                        </div>
-                        <button type="submit" :disabled="name ? false : true" :class="[
-                            'w-full h-9 bg-chatgpt-700 text-orange-900 text-center rounded-lg font-medium flex justify-center items-center',
-                            'enabled:hover:bg-orange-900 enabled:hover:text-gray-100']">
-                            <Spinner v-if="loading" /><span v-else>Delete this chat</span>
-                        </button>
-                    </div>
-                </form>
+                <div class="p-3">
+                    <button type="submit" @click="handleDelete" :class="[
+                        'w-full h-10 rounded-lg', 'text-center text-gray-200 font-medium text-sm',
+                        'flex justify-center items-center', 'bg-chatgpt-500 hover:bg-chatgpt-400'
+                    ]">
+                        <Spinner v-if="loading" /><span v-else>Delete this chat</span>
+                    </button>
+                </div>
             </div>
         </div>
     </teleport>
@@ -60,19 +49,14 @@ const loading = ref<boolean>(false)
 const store = useSideBarStoreAzureStore()
 const router = useRouter()
 
-async function handleSubmit(e: any) {
-    e.preventDefault()
-    const filtered = name.value.trim().replace(/\s\s/g, " ").toLowerCase()
-    const target = props.chat.name.trim().toLowerCase()
-    // console.log(filtered, "===", target);
-    if (target.includes(filtered)) {
-        loading.value = true
+async function handleDelete() {
+    loading.value = true
 
-        await store.deleteSideBarInstance(props.chat.id)
+    await store.deleteSideBarInstance(props.chat.id)
 
-        // navigate to welcome page when chat delete
-        router.push("/kraalai")
-    } else { err.value = true }
+    loading.value = false;
+    // navigate to welcome page when chat delete
+    router.push("/kraalai")
 }
 
 // on input change
