3e6f05fd05c4c24cda426122553dae250cd05f56
Santo <santokhan1999@gmail.com>
2023-12-24 00:16:04 +0600

    Refactor

--- src/components/kai/typewriter/markdownParser.ts
+++ src/components/kai/typewriter/markdownParser.ts
@@ -1,6 +1,14 @@
 import MarkdownIt from 'markdown-it'
 
-export default function markdownParser(data: string): string {
-    const MD = new MarkdownIt()
-    return MD.render(data)
+export type MarkDown = string;
+export type HTML = string;
+
+/**
+ * Parse markdown to html
+ * @param {string} data 
+ * @returns {string}
+ */
+export default function markdownParser(data: MarkDown): HTML {
+    const MD = new MarkdownIt();
+    return MD.render(data);
 }
