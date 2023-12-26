791f0d40b632e233eeecc7450c228b929f3350aa
Santo <santokhan1999@gmail.com>
2023-12-26 16:26:16 +0600

    Update profile

---
+++ src/views/user/ProfileView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="max-w-screen-xl mx-auto w-full px-4 text-noom-text-900">
+        <div class="w-full flex flex-wrap lg:flex-nowrap py-12 gap-5 items-start">
+            <div class="w-full lg:w-1/2 space-y-5">
+                <Company />
+                <PaymentStatusCard />
+            </div>
+            <div class="w-full lg:w-1/2 space-y-5">
+                <Admin />
+                <Notifications />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import PaymentStatusCard from '../../blocks/payment-status-card/PaymentStatusCard.vue';
+import Admin from '../../blocks/user/profile/Admin.vue';
+import Company from "../../blocks/user/profile/Company.vue";
+import Notifications from "../../blocks/user/notifications/Notifications.vue";
+</script>
