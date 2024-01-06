a33aa4c2a6d30ef703f192942370edf5d74e7d27
Santo <santokhan1999@gmail.com>
2024-01-07 02:30:35 +0600

    Update conversation UI

---
+++ src/components/kai/instances/chat-main/users/IconByInitialName.vue
@@ -0,0 +1,16 @@
+<template>
+    <div :class="[
+        'w-[1.95rem] min-w-[1.95rem] h-[1.95rem] rounded-full bg-gray-400',
+        'text-md font-bold text-chatgpt-500',
+        'grid place-items-center'
+    ]">
+        {{ props.name }}
+    </div>
+</template>
+
+<script setup lang="ts">
+type Name = string;
+const props = defineProps<{ name: Name }>();
+</script>
+
+<style scoped></style>
